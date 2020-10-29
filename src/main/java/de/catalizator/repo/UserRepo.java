bd4f3baddc0f5b5bf00dc1f3471a4f89d087badc
Ayrat <ayrat1@mail.ru>
2020-10-30 00:41:01 +0300

    3 complete, add Spring Security

---
+++ src/main/java/de/catalizator/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package de.catalizator.repo;
+
+import de.catalizator.domain.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepo extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByUsername(String name);
+}
