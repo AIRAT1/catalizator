e389929ed17c83cf7481641a4d921e44213a81ac
Ayrat <ayrat1@mail.ru>
2020-10-30 22:53:38 +0300

    3 complete jwt

--- src/main/java/de/catalizator/config/AuthenticationManager.java
+++ src/main/java/de/catalizator/config/AuthenticationManager.java
@@ -1,10 +1,16 @@
 package de.catalizator.config;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class AuthenticationManager implements ReactiveAuthenticationManager {
@@ -18,7 +24,27 @@ public class AuthenticationManager implements ReactiveAuthenticationManager {
     @Override
     public Mono<Authentication> authenticate(Authentication authentication) {
         String authToken = authentication.getCredentials().toString();
-        String username = jwtUtil.extractUsername(authToken);
-        return null;
+        String username;
+        try {
+            username = jwtUtil.extractUsername(authToken);
+        } catch (Exception e) {
+            username = null;
+            System.err.println(e);
+        }
+        if (username != null && jwtUtil.validateToken(authToken)) {
+            Claims claims = jwtUtil.getClaimsFromToken(authToken);
+            List<String> role = claims.get("role", List.class);
+            List<SimpleGrantedAuthority> authorities = role.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    authorities
+            );
+            return Mono.just(authenticationToken);// fixme or authentication?
+        } else {
+            return Mono.empty();
+        }
     }
 }
--- src/main/java/de/catalizator/config/WebSecurityConfig.java
+++ src/main/java/de/catalizator/config/WebSecurityConfig.java
@@ -2,12 +2,15 @@ package de.catalizator.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -29,6 +32,20 @@ public class WebSecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)
+                                )
+                )
+                .accessDeniedHandler(
+                        (swe, e) ->
+                                Mono.fromRunnable(
+                                        () -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN)
+                                )
+                )
+                .and()
                 .csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
